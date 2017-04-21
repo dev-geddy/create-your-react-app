df98c52506c415274e9995449c2c02e673f270e4
Gediminas <gediminasgn@gmail.com>
2017-04-21 16:36:38 +0100

    Creating simple webpage DOM structure containing header, navigation, body and footer.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        This will be my React application!
+        <header>
+          <h1>
+            My React Site
+          </h1>
+          <nav>
+            <ul>
+              <li><a>Home</a></li>
+              <li><a>Contact</a></li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          <article>
+            <h2>Home</h2>
+            <p>Hello people, let's build the React application!</p>
+          </article>
+        </section>
+        <footer>
+          Built to help people learn React! It's 2017 now.
+        </footer>
       </div>
     );
   }
