a603f8d5f138d204691c4316e4834a446105ed3f
Gediminas <gediminasgn@gmail.com>
2017-04-21 17:13:09 +0100

    Limiting content width with spacing from screen sides, retaining flexibility. Adding some vertical padding for footer.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>
-            My React Site
-          </h1>
-          <nav>
-            <ul>
-              <li><a>Home</a></li>
-              <li><a>Contact</a></li>
-            </ul>
-          </nav>
+          <div className="App-content-width">
+            <h1>
+              My React Site
+            </h1>
+            <nav>
+              <ul>
+                <li><a>Home</a></li>
+                <li><a>Contact</a></li>
+              </ul>
+            </nav>
+          </div>
         </header>
         <section className="App-body">
-          <article>
-            <h2>Home</h2>
-            <p>Hello people, let's build the React application!</p>
-          </article>
+          <div className="App-content-width">
+            <article>
+              <h2>Home</h2>
+              <p>Hello people, let's build the React application!</p>
+            </article>
+          </div>
         </section>
         <footer className="App-footer">
-          Built to help people learn React! It's 2017 now.
+          <div className="App-content-width">
+            Built to help people learn React! It's 2017 now.
+          </div>
         </footer>
       </div>
     );
