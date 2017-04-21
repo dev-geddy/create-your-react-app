afa0a58f2bdee38da494565745e78b155823a85e
Gediminas <gediminasgn@gmail.com>
2017-04-21 17:52:29 +0100

    Test updated App component. Install enzyme and react-test-renderer.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+// run "npm install react-test-renderer --save-dev" and "npm install enzyme --save-dev" to install testing tools.
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('contains Header and Footer components', () => {
+  const component = shallow(<App />);
+  expect(component.find('Header')).toBeDefined();
+  expect(component.find('Footer')).toBeDefined();
 });
