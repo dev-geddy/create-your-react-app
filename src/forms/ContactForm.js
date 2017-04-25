8082e31d7c9f0a5292a7bb35579ee4f2c44b728b
Gediminas <gediminasgn@gmail.com>
2017-04-25 11:58:00 +0100

    Add input value handling via local component state.

--- src/forms/ContactForm.js
+++ src/forms/ContactForm.js
@@ -2,20 +2,35 @@ import React, {Component} from 'react';
 
 export class ContactForm extends Component {
 
+  state = {
+    name: '',
+    email: '',
+    message: '',
+  }
+
   handleFormSubmit = (event) => {
     event.preventDefault()
-    console.log("handleFormSubmit()")
+    console.log("handleFormSubmit, form data is: ", this.state);
+  }
+
+  handleChange = (event) => {
+    console.log(`onChange for ${event.target.id}: ${event.target.value}`);
+    let newState = {
+      ...this.state
+    };
+    newState[event.target.id] = event.target.value;
+    this.setState(newState);
   }
 
   render() {
     return (
       <form onSubmit={this.handleFormSubmit}>
         <label htmlFor="name">Name:</label><br />
-        <input type="text" id="name"/><br />
+        <input type="text" id="name" onChange={this.handleChange}/><br />
         <label htmlFor="email">Email:</label><br />
-        <input type="email" id="email"/><br />
+        <input type="email" id="email" onChange={this.handleChange}/><br />
         <label htmlFor="message">Message:</label><br />
-        <textArea id="message" /><br />
+        <textArea id="message" onChange={this.handleChange}/><br />
         <button type="submit">Send it!</button>
       </form>
     );
