ee0639b2d4dd5c71e70bdca092eca8c040a5489f
Gediminas <gediminasgn@gmail.com>
2017-04-25 17:15:05 +0100

    Form reset added.

--- src/forms/ContactForm.js
+++ src/forms/ContactForm.js
@@ -16,7 +16,7 @@ export class ContactForm extends Component {
 
   handleChange = (event) => {
     console.log(`onChange for ${event.target.id}: ${event.target.value}`);
-    const { id, value } = event.target;
+    const {id, value} = event.target;
     let newState = {
       ...this.state
     };
@@ -26,23 +26,35 @@ export class ContactForm extends Component {
     this.setState(newState);
   }
 
+  resetForm = (event) => {
+    event.preventDefault();
+    this.setState({
+      name: '',
+      email: '',
+      message: ''
+    })
+  }
+
   render() {
+    const {name, email, message} = this.state;
+
     return (
       <form onSubmit={this.handleFormSubmit}>
         <div className="ContactForm-row">
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" onChange={this.handleChange}/>
+          <input type="text" id="name" onChange={this.handleChange} value={name}/>
         </div>
         <div className="ContactForm-row">
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" onChange={this.handleChange}/>
+          <input type="email" id="email" onChange={this.handleChange} value={email}/>
         </div>
         <div className="ContactForm-row">
           <label htmlFor="message">Message:</label>
-          <textArea id="message" onChange={this.handleChange}/>
+          <textArea id="message" onChange={this.handleChange} value={message}/>
         </div>
         <div className="ContactForm-row">
           <button type="submit">Send it!</button>
+          <button onClick={this.resetForm}>Reset</button>
         </div>
       </form>
     );
