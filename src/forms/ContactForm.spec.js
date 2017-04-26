05869fc339a52fbb021b15e78d9ddfcaf7d6f9d3
Gediminas <gediminasgn@gmail.com>
2017-04-26 11:27:02 +0100

    Reset button test added. Submit test udpated.

--- src/forms/ContactForm.spec.js
+++ src/forms/ContactForm.spec.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ContactForm from './ContactForm';
 import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
+it('should render without crashing', () => {
   shallow(<ContactForm />);
 });
 
-it('has all inputs', () => {
+it('should contain all inputs', () => {
   const component = shallow(<ContactForm />);
   expect(component.find('input').length).toEqual(2);
   expect(component.find('textArea').length).toEqual(1);
   expect(component.find('button').length).toEqual(2);
 });
 
-it('name input updates the state', () => {
+it('name input should update the state', () => {
   const component = mount(<ContactForm />);
   const mockChangeEvent = {
     target: {
@@ -25,7 +25,7 @@ it('name input updates the state', () => {
   expect(component.state().name).toEqual(mockChangeEvent.target.value);
 });
 
-it('email input updates the state', () => {
+it('email input should update the state', () => {
   const component = mount(<ContactForm />);
   const mockChangeEvent = {
     target: {
@@ -37,7 +37,7 @@ it('email input updates the state', () => {
   expect(component.state().email).toEqual(mockChangeEvent.target.value);
 });
 
-it('message textarea updates the state', () => {
+it('message textarea should update the state', () => {
   const component = mount(<ContactForm />);
   const mockChangeEvent = {
     target: {
@@ -49,7 +49,7 @@ it('message textarea updates the state', () => {
   expect(component.state().message).toEqual(mockChangeEvent.target.value);
 });
 
-it('form submit will call handleFormSubmit function', () => {
+it('form submit should call handleFormSubmit function', () => {
   const component = mount(<ContactForm />);
   const mockEvent = {
     preventDefault: jest.fn()
@@ -59,8 +59,26 @@ it('form submit will call handleFormSubmit function', () => {
     email: 'someone@some123domain.com',
     message: 'Hello world'
   };
-  spyOn(component.nodes[0], 'handleFormSubmit');
+  spyOn(component.nodes[0], 'handleFormSubmit').and.callThrough();
   component.setState({...mockFormData});
   component.find('form').at(0).simulate("submit", mockEvent);
   expect(component.nodes[0].handleFormSubmit).toBeCalled();
+  expect(mockEvent.preventDefault).toBeCalled();
+});
+
+it('reset button should clear the form', () => {
+  const component = mount(<ContactForm />);
+  const mockEvent = {
+    preventDefault: jest.fn()
+  };
+  const mockFormData = {
+    name: 'John',
+    email: 'someone@some123domain.com',
+    message: 'Hello world'
+  };
+  spyOn(component.nodes[0], 'resetForm').and.callThrough();
+  component.setState({...mockFormData});
+  component.find('button').at(1).simulate("click", mockEvent);
+  expect(component.nodes[0].resetForm).toBeCalled();
+  expect(component.state()).not.toEqual(mockFormData);
 });
