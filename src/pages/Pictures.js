218c1c8d9b20f25ce594144cf1770fefbea474c3
Gediminas <gediminasgn@gmail.com>
2017-05-17 10:29:53 +0100

    Sample pictures and basic responsive representation.

--- src/pages/Pictures.js
+++ src/pages/Pictures.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import pictures from '../sampleData/pictures'
+import './Pictures.css'
+
+const RenderPictures = (picturesList) => {
+  return picturesList.map((picture) => {
+    return (
+      <div className="Picture">
+        <img src={picture.url} alt={picture.name} />
+      </div>
+    )
+  })
+}
 
 const Pictures = () => {
   return {
@@ -8,6 +20,7 @@ const Pictures = () => {
           <article>
             <h2>Pictures</h2>
             <p>Simple picture gallery.</p>
+            <div className="Pictures-gallery">{RenderPictures(pictures)}</div>
           </article>
         </div>
       )
---
+++ src/sampleData/pictures.js
@@ -0,0 +1,20 @@
+const pictures = [
+  {
+    name: "Landscape",
+    url: "/images/picture-00.png",
+  },
+  {
+    name: "Bird's Eye View",
+    url: "/images/picture-00.png",
+  },
+  {
+    name: "Vehicle",
+    url: "/images/picture-00.png",
+  },
+  {
+    name: "Clouds",
+    url: "/images/picture-00.png",
+  }
+]
+
+export default pictures
